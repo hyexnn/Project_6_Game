3af5ad159012a1a47f99652c83e71553aac1a8d6
hyeonzz2795@gmail.com <codnjs0801*>
2024-11-11 01:20:34 +0900

    스크립트추가

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public int dmg;
-    public bool isRotate;
 
-    void Update()
+    public float speed = 8f;                    //탄알 이동 속력
+    private Rigidbody bulletRigidbody;          //이동에 사용할 이지드바디 컴포넌트
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        if (isRotate)
-            transform.Rotate(Vector3.forward * 10);
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    void OnTriggerEnter(Collider other)         //트리거 충돌 시 자동으로 실행되는 매서드
     {
-        if(collision.gameObject.tag == "BorderBullet")
+        if (other.tag == "Target")
         {
-            gameObject.SetActive(false);
+            Target_Move Target_Move = other.GetComponent<Target_Move>();
+            //상대방 게임 오브젝트에서 playerController 컴포넌트 가져오기
+
+            if (Target_Move != null)       //상대방으로 부터 PlayerController 컴포넌트를 가져오는 데 성공했다면
+            {
+                Target_Move.Die();         //상대방 PlayerController 컴포넌트 Die() 매서드실행
+            }
         }
     }
 }
